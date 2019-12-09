41e22307737e4a28087e761e0666cf765fa4f8d8
Tempelaere Tom <tempelaere.t@gmail.com>
2019-12-09 16:30:22 +0100

    cssRegister

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php require "head.php"; ?>
+
+<body>
+    <div class="formulaire">
+
+        <h1>TINCAT</h1>
+        <h2>Inscrire son chat</h2>
+        <form action="" class="form_container">
+            <input type="text" placeholder="pseudo">
+            <input type="text" placeholder="password">
+            <input type="submit" value="register">
+        </form>
+    </div>
+</body>
+</html>
